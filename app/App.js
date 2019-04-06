42199af8382d1179916f9a487673b17d01e92b84
Chad Ostrowski <hi@chadoh.com>
2019-04-06 12:50:54 -0400

    Move initial data to new database object
    This pattern borrowed from a previous project; it will require
    significant modification but it's a decent starting point.

--- app/App.js
+++ app/App.js
@@ -7,42 +7,9 @@ import {
 } from '@aragon/ui'
 import Aragon, { providers } from '@aragon/client'
 import styled from 'styled-components'
+import createDatabase from './database'
 
-const db = {
-  widgets: {
-    data: {
-      manifesto: {
-        title: "Manifesto",
-        body: `The history of all hitherto existing society is the history of class struggles.
-
-Freeman and slave, patrician and plebeian, lord and serf, guild-master and journeyman, in a word, oppressor and oppressed, stood in constant opposition to one another, carried on an uninterrupted, now hidden, now open fight, a fight that each time ended, either in a revolutionary reconstitution of society at large, or in the common ruin of the contending classes.`,
-      },
-      values: {
-        title: "Values",
-        body: `1. Individuals and interactions over processes and tools
-2. Working software over comprehensive documentation
-3. Customer collaboration over contract negotiation
-4. Responding to change over following a plan`
-      },
-      coc: {
-        title: "Code of Conduct",
-        body: `## Our Pledge
-
-In the interest of fostering an open and welcoming environment, we as
-contributors and maintainers pledge to making participation in our project and
-our community a harassment-free experience for everyone, regardless of age, body
-size, disability, ethnicity, sex characteristics, gender identity and expression,
-level of experience, education, socio-economic status, nationality, personal
-appearance, race, religion, or sexual identity and orientation.`,
-      },
-      contact: {
-        title: "Contact Info",
-        body: "[@chadoh](https://twitter.com/chadoh)",
-      }
-    },
-    order: ["manifesto", "values", "coc", "contact"],
-  }
-}
+const db = createDatabase();
 
 const Grid = styled.div`
   display: grid;
@@ -51,13 +18,16 @@ const Grid = styled.div`
 `
 
 export default class App extends React.Component {
+  state = {
+    widgets: db.fetchData().widgets
+  }
   render () {
     return (
       <Main>
         <AppView title="Customize Your DAO">
           <Grid>
-            {db.widgets.order.map(id => {
-              const { title, body } = db.widgets.data[id]
+            {this.state.widgets.order.map(id => {
+              const { title, body } = this.state.widgets.data[id]
               return (
                 <Card key={id} height="initial" width="initial">
                   <div style={{padding: "1em"}}>
---
+++ app/database/initialState.js
@@ -0,0 +1,35 @@
+export default {
+  widgets: {
+    data: {
+      manifesto: {
+        title: "manifesto",
+        body: `the history of all hitherto existing society is the history of class struggles.
+
+freeman and slave, patrician and plebeian, lord and serf, guild-master and journeyman, in a word, oppressor and oppressed, stood in constant opposition to one another, carried on an uninterrupted, now hidden, now open fight, a fight that each time ended, either in a revolutionary reconstitution of society at large, or in the common ruin of the contending classes.`,
+      },
+      values: {
+        title: "values",
+        body: `1. individuals and interactions over processes and tools
+2. working software over comprehensive documentation
+3. customer collaboration over contract negotiation
+4. responding to change over following a plan`
+      },
+      coc: {
+        title: "code of conduct",
+        body: `## our pledge
+
+in the interest of fostering an open and welcoming environment, we as
+contributors and maintainers pledge to making participation in our project and
+our community a harassment-free experience for everyone, regardless of age, body
+size, disability, ethnicity, sex characteristics, gender identity and expression,
+level of experience, education, socio-economic status, nationality, personal
+appearance, race, religion, or sexual identity and orientation.`,
+      },
+      contact: {
+        title: "contact info",
+        body: "[@chadoh](https://twitter.com/chadoh)",
+      }
+    },
+    order: ["manifesto", "values", "coc", "contact"],
+  }
+}
