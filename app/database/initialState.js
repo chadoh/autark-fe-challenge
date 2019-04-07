11e59e680997b395ea26bc901b923eb8098575db
Chad Ostrowski <hi@chadoh.com>
2019-04-06 20:34:39 -0400

    Use uuids for widget ids

--- app/database/initialState.js
+++ app/database/initialState.js
@@ -1,20 +1,29 @@
+import uuid from "uuid/v4"
+
+const ids = [
+  uuid(),
+  uuid(),
+  uuid(),
+  uuid(),
+]
+
 export default {
   widgets: {
     data: {
-      manifesto: {
+      [ids[0]]: {
         title: "manifesto",
         body: `the history of all hitherto existing society is the history of class struggles.
 
 freeman and slave, patrician and plebeian, lord and serf, guild-master and journeyman, in a word, oppressor and oppressed, stood in constant opposition to one another, carried on an uninterrupted, now hidden, now open fight, a fight that each time ended, either in a revolutionary reconstitution of society at large, or in the common ruin of the contending classes.`,
       },
-      values: {
+      [ids[1]]: {
         title: "values",
         body: `1. individuals and interactions over processes and tools
 2. working software over comprehensive documentation
 3. customer collaboration over contract negotiation
 4. responding to change over following a plan`
       },
-      coc: {
+      [ids[2]]: {
         title: "code of conduct",
         body: `### our pledge
 
@@ -25,11 +34,11 @@ size, disability, ethnicity, sex characteristics, gender identity and expression
 level of experience, education, socio-economic status, nationality, personal
 appearance, race, religion, or sexual identity and orientation.`,
       },
-      contact: {
+      [ids[3]]: {
         title: "contact info",
         body: "[@chadoh](https://twitter.com/chadoh)",
       }
     },
-    order: ["manifesto", "values", "coc", "contact"],
+    order: ids,
   }
 }
